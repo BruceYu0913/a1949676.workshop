6c7f04399ce8b73b6101c91d143677bfedfb4a5a
BruceYu0913 <a1949676@adelaide.edu.au>
2026-08-11 11:59:52 +0930

    Add PracA

---
+++ week02/PracA/PayrollCalculator/Payroll.cs
@@ -0,0 +1,37 @@
+﻿class Payroll
+{
+    private double Hours { get; set; }
+    private double Rate { get; set; }
+    private double TaxRate { get; set; }
+
+    public double CalculateNetPay()
+    {
+        double gross = Hours * Rate;
+        double tax = gross * TaxRate;
+        double net = gross - tax;
+
+        return net;
+    }
+
+    public void ChangeTaxRate(double newTaxRate)
+    {
+        TaxRate = newTaxRate;
+    }
+
+    public Payroll(
+        double hours,
+        decimal rate,
+        decimal taxRate
+    )
+    {
+        Hours = hours;
+        Rate = (double)rate;
+        TaxRate = (double)taxRate;
+
+        double netPay = CalculateNetPay();
+
+        Console.WriteLine(
+            $"Net pay after tax: {netPay:F2}"
+        );
+    }
+}
