651bbf419813dad94ce3fa31302b63941454c7b2
BruceYu0913 <a1949676@adelaide.edu.au>
2026-09-01 10:48:32 +0930

    Task1

---
+++ week05/PracA/Reflection/Note.md
@@ -0,0 +1,3 @@
+Task 1 
+
+In this task, I used a string array and a foreach loop to print the names and compare their Length values. Bruce was the longest name, while Ayo was the shortest. I used Array.Sort() to sort the original array alphabetically. Array.Sort() modifies the array directly and uses an introspective sorting algorithm. Because my array is small, insertion sort is used. I then used Array.Reverse() to reverse the sorted array in place.
---
+++ week05/PracA/task/Program.cs
@@ -0,0 +1,66 @@
+﻿class Program
+{
+    private static void Main()
+    {
+        string[] names =
+        {
+            "Bruce","Tony","Josh","Ayo"
+        };
+
+        Console.WriteLine("All names:");
+        PrintArray(names);
+
+        string longestName = names[0];
+        string shortestName = names[0];
+
+        foreach (string name in names)
+        {
+            if (name.Length > longestName.Length)
+            {
+                longestName = name;
+            }
+
+            if (name.Length < shortestName.Length)
+            {
+                shortestName = name;
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine(
+            $"Longest name: {longestName}"
+        );
+
+        Console.WriteLine(
+            $"Shortest name: {shortestName}"
+        );
+
+        Console.WriteLine();
+        Console.WriteLine("Before sorting:");
+        PrintArray(names);
+
+        Array.Sort(names);
+
+        Console.WriteLine();
+        Console.WriteLine("After sorting:");
+        PrintArray(names);
+
+        Console.WriteLine();
+        Console.WriteLine("Before reversing:");
+        PrintArray(names);
+
+        Array.Reverse(names);
+
+        Console.WriteLine();
+        Console.WriteLine("After reversing:");
+        PrintArray(names);
+    }
+
+    private static void PrintArray(string[] names)
+    {
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
