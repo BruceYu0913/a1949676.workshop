d984bcb16f37437f7a91e57b361ff9beef7e905f
BruceYu0913 <a1949676@adelaide.edu.au>
2026-09-08 11:25:11 +0930

    Task1&2

---
+++ week06/PracA/queue/Program.cs
@@ -0,0 +1,71 @@
+﻿class Program
+{
+    private static void Main()
+    {
+        Queue<string> printQueue =
+            new Queue<string>();
+
+        PrintQueue("Initial queue:", printQueue);
+
+        printQueue.Enqueue("Print Job 1");
+        PrintQueue(
+            "After Enqueue(\"Print Job 1\"):",
+            printQueue
+        );
+
+        printQueue.Enqueue("Print Job 2");
+        PrintQueue(
+            "After Enqueue(\"Print Job 2\"):",
+            printQueue
+        );
+
+        printQueue.Enqueue("Print Job 3");
+        PrintQueue(
+            "After Enqueue(\"Print Job 3\"):",
+            printQueue
+        );
+
+        string nextJob = printQueue.Peek();
+
+        Console.WriteLine();
+        Console.WriteLine($"Peek result: {nextJob}");
+
+        PrintQueue(
+            "Queue after Peek():",
+            printQueue
+        );
+
+        string completedJob = printQueue.Dequeue();
+
+        Console.WriteLine();
+        Console.WriteLine(
+            $"Dequeue result: {completedJob}"
+        );
+
+        PrintQueue(
+            "Queue after Dequeue():",
+            printQueue
+        );
+    }
+
+    private static void PrintQueue(
+        string title,
+        Queue<string> printQueue
+    )
+    {
+        Console.WriteLine();
+        Console.WriteLine(title);
+
+        if (printQueue.Count == 0)
+        {
+            Console.WriteLine("(empty)");
+        }
+        else
+        {
+            foreach (string job in printQueue)
+            {
+                Console.WriteLine(job);
+            }
+        }
+    }
+}
---
+++ week06/PracA/stack/Program.cs
@@ -0,0 +1,79 @@
+﻿class Program
+{
+    private static void Main()
+    {
+        Stack<string> undoHistory =
+            new Stack<string>();
+
+        PrintStack(
+            "Initial stack:",
+            undoHistory
+        );
+
+        undoHistory.Push("Typed Text");
+
+        PrintStack(
+            "After Push(\"Typed Text\"):",
+            undoHistory
+        );
+
+        undoHistory.Push("Inserted Image");
+
+        PrintStack(
+            "After Push(\"Inserted Image\"):",
+            undoHistory
+        );
+
+        undoHistory.Push("Changed Colour");
+
+        PrintStack(
+            "After Push(\"Changed Colour\"):",
+            undoHistory
+        );
+
+        string topAction = undoHistory.Peek();
+
+        Console.WriteLine();
+        Console.WriteLine(
+            $"Peek result: {topAction}"
+        );
+
+        PrintStack(
+            "Stack after Peek():",
+            undoHistory
+        );
+
+        string removedAction = undoHistory.Pop();
+
+        Console.WriteLine();
+        Console.WriteLine(
+            $"Pop result: {removedAction}"
+        );
+
+        PrintStack(
+            "Stack after Pop():",
+            undoHistory
+        );
+    }
+
+    private static void PrintStack(
+        string title,
+        Stack<string> undoHistory
+    )
+    {
+        Console.WriteLine();
+        Console.WriteLine(title);
+
+        if (undoHistory.Count == 0)
+        {
+            Console.WriteLine("(empty)");
+        }
+        else
+        {
+            foreach (string action in undoHistory)
+            {
+                Console.WriteLine(action);
+            }
+        }
+    }
+}
